19ca9f9b2356ce803b16b8a52028784c0ebb2abc
BöbGames <96718342+BobGaemes@users.noreply.github.com>
2023-12-12 15:46:37 +0100

    Add files via upload

---
+++ import.js
@@ -0,0 +1,26 @@
+const targetEL = document.getElementById('target');
+const footerEL = document.getElementById('footer');
+
+fetch('/base.html')
+  .then(res => {
+    if (res.ok) {
+      return res.text();
+    }
+  })
+  .then(htmlSnippet => {
+    targetEL.innerHTML = htmlSnippet;
+  });
+
+fetch('/footer.html')
+  .then(res => {
+    if (res.ok) {
+      return res.text();
+    }
+  })
+  .then(htmlSnippet => {
+    footerEL.innerHTML = htmlSnippet;
+  });
+
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  location.replace("https://phone.bobgaemes.repl.co");
+}
