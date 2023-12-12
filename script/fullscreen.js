722c70d8b58c543699953f7db0c08a8fb7d4e2eb
BöbGames <96718342+BobGaemes@users.noreply.github.com>
2023-12-12 16:07:28 +0100

    Add files via upload

---
+++ script/fullscreen.js
@@ -0,0 +1,20 @@
+var GAME_IFRAME;
+
+window.addEventListener("load", (event) => {
+  GAME_IFRAME = document.getElementById("game-iframe");
+});
+
+function fullscreen() {
+
+  GAME_IFRAME.style.borderRadius = "25px 25px 25px 25px";
+
+  if (GAME_IFRAME.requestFullscreen) {
+    GAME_IFRAME.requestFullscreen();
+  } else if (GAME_IFRAME.webkitRequestFullscreen) { /* Safari */
+    GAME_IFRAME.webkitRequestFullscreen();
+  } else if (GAME_IFRAME.msRequestFullscreen) { /* IE11 */
+    GAME_IFRAME.msRequestFullscreen();
+  }
+
+  GAME_IFRAME.style.borderRadius = "20px 20px 0px 0px";
+}
